3e284b1ee1af9e68a90dd360019a5e3e0ac59bdc
VladyslavSh <shimanovskiyvlad0@gmail.com>
2024-11-21 17:29:34 +0200

    added typehinting

--- colouring_graphs.py
+++ colouring_graphs.py
@@ -5,20 +5,29 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def reading_json_file(filename):
+def reading_json_file(filename: str) -> dict:
     """
-    Reads files
+    Reads json files with information about nodes of the graph
+
+    :param filename: str, file from which we read an info
+    :return: dict, dictionary with nodes as a key and info about them as a value
+
+    >>> reading_json_file("test_json_1.json")
+    {'1': {'color': 'b', 'edge_with': [2, 3, 4]}, '2': {'color': 'b',\
+ 'edge_with': [1, 4]}, '3': {'color': 'g', 'edge_with': [1]},\
+ '4': {'color': 'r', 'edge_with': [1, 2]}}
     """
     with open(filename, 'r', encoding = 'utf-8') as file:
         result = json.load(file)
     return result
 
 
-def draw_graph(data):
+def draw_graph(data: dict) -> None:
     """
-    Visualizes the graph based on the json file data.
-    
-    :data (dict): A dictionary containing data.
+    Visualizes the graph based on the graph data.
+
+    :param data: dict, A dictionary containing data.
+    :return: None, shows the vizualization of the graph
     """
     G = nx.Graph()
     for node, info in data.items():
@@ -34,7 +43,7 @@ def draw_graph(data):
 
 
 if __name__ == '__main__':
-    print(reading_json_file('test_json_1.json'))
-    filename = 'test_json_2.json'
-    data = reading_json_file(filename)
-    draw_graph(data)
+    import doctest
+    print(doctest.testmod())
+
+    draw_graph(reading_json_file('test_json_2.json'))
