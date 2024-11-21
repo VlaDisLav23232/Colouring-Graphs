0cc55e771225214a77a1e80e49a46605292b87c6
VladyslavSh <shimanovskiyvlad0@gmail.com>
2024-11-21 21:43:19 +0200

    Added 2 test files and main algorithm

--- colouring_graphs.py
+++ colouring_graphs.py
@@ -22,6 +22,53 @@ def reading_json_file(filename: str) -> dict:
     return result
 
 
+def drawing_with_new_colours(data: dict, num: int = 1, new_dict: None|dict = None) -> dict:
+    """
+    Draws graph nodes with new colours, by taking into account, that
+    two neighbouring nodes cannot be identically coloured and node cannot
+    be coloured with its previous colour.
+
+    :param data: dict, A dictionary containing data.
+    :param num: int, graph node number
+    :param new_dict: None or dict, None at the recursion depth 1 and
+    dict at the recursion depth with 2 or more. Saves the data about recoloured graph
+    :return: dict, recoloured graph
+
+    >>> drawing_with_new_colours({'1': {'color': 'b', 'edge_with': [2, 3, 4]},\
+ '2': {'color': 'r', 'edge_with': [1, 3]}, '3': {'color': 'r', 'edge_with':\
+ [1, 2, 4]}, '4': {'color': 'r', 'edge_with': [1, 3]}})
+    {'1': {'color': 'r', 'edge_with': [2, 3, 4]}, '2': {'color': 'b', 'edge_with':\
+ [1, 3]}, '3': {'color': 'g', 'edge_with': [1, 2, 4]}, '4':\
+ {'color': 'b', 'edge_with': [1, 3]}}
+    """
+
+    if new_dict is None:
+        new_dict = {}
+
+    if len(data) < num:
+        return new_dict
+
+    available_colours = ["r", "b", "g"]
+    available_colours.remove(data[str(num)]["color"])
+    new_dict.setdefault(str(num), {"color": "", "edge_with": data[str(num)]["edge_with"]})
+
+    for el in data[str(num)]["edge_with"]:
+        new_dict.setdefault(str(el), {"color": "", "edge_with": data[str(el)]["edge_with"]})
+        if new_dict[str(el)]["color"] in available_colours and num > el:
+            available_colours.remove(new_dict[str(el)]["color"])
+
+    for colour in available_colours:
+        new_dict[str(num)]["color"] = colour
+        print(available_colours)
+        new_dict = drawing_with_new_colours(data, num+1, new_dict)
+        if new_dict[str(len(new_dict))]["color"] != "":
+            break
+
+    if num == 1 and new_dict[str(len(new_dict))]["color"] == "":
+        return None
+
+    return new_dict
+
 def draw_graph(data: dict) -> None:
     """
     Visualizes the graph based on the graph data.
@@ -29,15 +76,16 @@ def draw_graph(data: dict) -> None:
     :param data: dict, A dictionary containing data.
     :return: None, shows the vizualization of the graph
     """
-    G = nx.Graph()
+
+    graph = nx.Graph()
     for node, info in data.items():
-        G.add_node(node, color=info["color"])
-        G.add_edges_from((node, str(neighbor)) for neighbor in info["edge_with"])
+        graph.add_node(node, color=info["color"])
+        graph.add_edges_from((node, str(neighbor)) for neighbor in info["edge_with"])
 
     nx.draw_circular(
-        G,
+        graph,
         with_labels=True,
-        node_color=[G.nodes[node]['color'] for node in G],
+        node_color=[graph.nodes[node]['color'] for node in graph],
     )
     plt.show()
 
@@ -46,4 +94,4 @@ if __name__ == '__main__':
     import doctest
     print(doctest.testmod())
 
-    draw_graph(reading_json_file('test_json_2.json'))
+    draw_graph(drawing_with_new_colours(reading_json_file('test_json_3.json')))
