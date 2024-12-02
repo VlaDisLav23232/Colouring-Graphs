dd4aa41b5640a2d614f50af54652e23c86f910c9
dianalyzenko <lyzenko.pn@ucu.edu.ua>
2024-12-02 19:27:10 +0200

    Update testing_prog.py

--- testing_prog.py
+++ testing_prog.py
@@ -21,17 +21,22 @@ def arguments():
 
 
 def run_program():
+    '''
+    Runs the program: processes graph data, and optionally visualizes it.
+    '''
     args = arguments()
 
     if not args.input_file.endswith(".json"):
         raise argparse.ArgumentTypeError(f"The file {args.input_file} is not a JSON file!")
-    
+
     data = reading_json_file(args.input_file)
     recolored_data = drawing_with_new_colours(data)
 
-    print("Recolored graph data:")
-    
+    if recolored_data is None:
+        print("The graph cannot be recolored!")
+
     if args.draw:
+        draw_graph(data)
         draw_graph(recolored_data)
 
 
